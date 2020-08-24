6c7ef645119324f6b33989d462f0587957c5f361
Arpan Patel <arpanvpatel61@gmail.com>
2020-08-24 11:00:01 -0700

    Lesson-5 JWT authentcation

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -16,10 +16,62 @@ export const verifyToken = token =>
     })
   })
 
-export const signup = async (req, res) => {}
+export const signup = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.send(400).send({ message: 'Email and Password Required!' })
+  }
 
-export const signin = async (req, res) => {}
+  try {
+    const user = await User.create(req.body)
+    const usertoken = newToken(user)
+    return res.status(201).send({ usertoken })
+  } catch (e) {
+    return res.status(400).end()
+  }
+}
+
+export const signin = async (req, res) => {
+  if (!req.body.email || !req.body.password) {
+    return res.send(400).send({ message: 'Email and Password Required!' })
+  }
+
+  const user = await User.findOne({ email: req.body.email })
+  if (!user) {
+    return res.status(401).send({ message: 'no auth!' })
+  }
+  try {
+    const match = user.checkPassword(req.body.password)
+    if (!match) {
+      return res.status(401).send({ message: 'no auth!' })
+    }
+  } catch (e) {
+    return res.status(401).send({ message: 'no auth!' })
+  }
+
+  const ntoken = newToken(user)
+  res.status(200).send({ ntoken })
+}
 
 export const protect = async (req, res, next) => {
-  next()
+  if (!req.headers.authorization) {
+    res.status(401).end()
+  }
+
+  let token = req.headers.authorization.split('Bearer ')[1]
+
+  if (!token) {
+    res.status(401).end()
+  }
+
+  try {
+    const payload = await verifyToken(token)
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+    req.user = user
+    next()
+  } catch (e) {
+    res.status(401).end()
+  }
 }
